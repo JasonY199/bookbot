ef0539f06e6dcc40883dd7233f0a45d267315aa2
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-10-05 21:07:24 -0400

    all chars in text is also calculated and outputted now

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
   num_words = get_num_words(text)
+  chars_dict = get_chars_dict(text)
+
+  # Print all outputs
   print(f"{num_words} words found in the document")
+  print(chars_dict)
 
 def get_book_text(path):
   with open(path) as f:
@@ -11,5 +15,15 @@ def get_book_text(path):
 def get_num_words(text):
   words = text.split()
   return len(words)
+
+def get_chars_dict(text):
+  chars = {}
+  for c in text:
+    lowered = c.lower()
+    if lowered not in chars:
+      chars[lowered] = 1
+    else:
+      chars[lowered] += 1
+  return chars
 
 main()
