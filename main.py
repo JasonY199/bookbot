a71546af1ceee8ce68afc9ad949b8953fdab5cd4
Jason Y <110353075+JasonY199@users.noreply.github.com>
2024-10-05 21:21:33 -0400

    char list is sorted and outputs in a clean report

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ def main():
   text = get_book_text(book_path)
   num_words = get_num_words(text)
   chars_dict = get_chars_dict(text)
+  chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-  # Print all outputs
-  print(f"{num_words} words found in the document")
-  print(chars_dict)
+  print(f"--- Begin report of {book_path} ---")
+  print(f"{num_words} words found in the document\n")
+  
+  for item in chars_sorted_list:
+    if not item["char"].isalpha():
+      continue
+    print(f"The '{item['char']}' character was found {item['num']} times")
+
+  print("--- End report ---")
 
 def get_book_text(path):
   with open(path) as f:
@@ -19,11 +26,23 @@ def get_num_words(text):
 def get_chars_dict(text):
   chars = {}
   for c in text:
-    lowered = c.lower()
-    if lowered not in chars:
-      chars[lowered] = 1
-    else:
-      chars[lowered] += 1
+    if c.isalpha():
+      lowered = c.lower()
+      if lowered not in chars:
+        chars[lowered] = 1
+      else:
+        chars[lowered] += 1
+  
   return chars
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
